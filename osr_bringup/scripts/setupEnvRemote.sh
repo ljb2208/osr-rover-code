095979bf89433275f452fd15f8080de61f78928f
Luke Barnett <ljb2208@gmail.com>
2019-11-19 18:14:31 -0500

    Add xavier launch file and scripts for env variables

---
+++ osr_bringup/scripts/setupEnvRemote.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# source this when you launch everything on a local machine
+export ROS_MASTER_URI=http://xavier-osr:11311
+export ROSLAUNCH_SSH_UNKNOWN=0
---
+++ osr_bringup/scripts/setupEnvXavier.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# source this when you launch everything on a local machine
+export ROS_MASTER_URI=http://localhost:11311
+export ROSLAUNCH_SSH_UNKNOWN=0
