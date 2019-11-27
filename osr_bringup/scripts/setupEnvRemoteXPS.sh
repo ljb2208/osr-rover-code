70fd9556c1fccee33f88f75b3af79597c1dc46c0
Luke Barnett <ljb2208@gmail.com>
2019-11-27 14:49:38 -0500

    changes to launch files and scripts initial commit of odometry node

--- osr_bringup/scripts/setupEnvRemoteXPS.sh
+++ osr_bringup/scripts/setupEnvRemoteXPS.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-source /home/pi/catkin_ws/devel/setup.sh
+#conda deactivate
+source /home/lbarnett/catkin_ws/devel/setup.sh
 
 # source this when you launch everything on a local machine
 export ROS_MASTER_URI=http://xavier-osr:11311
-export ROSLAUNCH_SSH_UNKNOWN=1
+export ROSLAUNCH_SSH_UNKNOWN=0
 
 exec "$@"
