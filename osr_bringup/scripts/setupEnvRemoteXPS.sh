d7b547225a81e81380919c92761b49683d221605
Luke Barnett <ljb2208@gmail.com>
2020-04-30 09:05:40 -0400

    Fix launch files and scripts Add EKF node launch file

--- osr_bringup/scripts/setupEnvRemoteXPS.sh
+++ osr_bringup/scripts/setupEnvRemoteXPS.sh
@@ -5,6 +5,6 @@ source /home/lbarnett/catkin_ws/devel/setup.sh
 
 # source this when you launch everything on a local machine
 export ROS_MASTER_URI=http://xavier-osr:11311
-export ROSLAUNCH_SSH_UNKNOWN=1
+export ROSLAUNCH_SSH_UNKNOWN=0
 
 exec "$@"
--- osr_bringup/scripts/setupEnvXavier.sh
+++ osr_bringup/scripts/setupEnvXavier.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
+source /home/lbarnett/catkin_ws/devel/setup.sh
 
 # source this when you launch everything on a local machine
 export ROS_MASTER_URI=http://localhost:11311
-export ROSLAUNCH_SSH_UNKNOWN=1
+export ROSLAUNCH_SSH_UNKNOWN=0
+
+exec "$@"
