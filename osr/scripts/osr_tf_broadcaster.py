a65f2b6b780eb0a993aba413d86d33331ae194bc
Luke Barnett <ljb2208@gmail.com>
2020-05-08 04:54:01 -0400

    More changes to TF

--- osr/scripts/osr_tf_broadcaster.py
+++ osr/scripts/osr_tf_broadcaster.py
@@ -36,7 +36,7 @@ class TfBroadCaster():
         self.tfList = [OSRTf("base_link", "imu_link", 0.12, 0, 0.50, 0, 0, 0)]
         self.tfList.append(OSRTf("base_link", "depth_camera_link", 0.14, 0, 0.45, 0, 0, 0))
         self.tfList.append(OSRTf("base_link", "tracking_camera_link", 0.14, 0, 0.39, 0, 0, 0))
-        self.tfList.append(OSRTf("tracking_camera_pose_frame", "base_link", -0.14, 0, -0.39, 0, 0, 0))  
+        # self.tfList.append(OSRTf("tracking_camera_pose_frame", "base_link", -0.14, 0, -0.39, 0, 0, 0))  
         # self.tfList.append(OSRTf("odom_combined", "base_link", 0, 0, 0, 0, 0, 0))  
 
         # self.tfList.append(OSRTf("tracking_camera_link", "tracking_camera_odom_frame", 0, 0, 0, 0, 0, 0))
