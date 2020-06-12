abc1689a2cabbeb89c813f60f97385a0f02cb57f
Luke Barnett <ljb2208@gmail.com>
2020-06-12 10:38:33 -0400

    remove astar implementation from master branch

--- osr_planning/scripts/collisiondetection.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python2
-import rospy
-from nav_msgs.msg import OccupancyGrid
-
-
-class CollisionDetection():
-    def __init__(self):
-        self.grid = OccupancyGrid()
-    
--- osr_planning/scripts/tf_broadcaster.py
+++
@@ -1,7 +0,0 @@
-import rospy
-
-
-if __name__ == '__main__':
-    rospy.init_node('tf_broadcaster')
-
-    rospy.spin()
